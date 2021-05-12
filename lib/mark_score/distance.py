ab3d7a56453310386e5fc09309d83a3b55602002
Admin <haidang.maicogroup@gmail.com>
2021-05-12 17:34:50 +0700

    upload branch mark_score

--- lib/mark_score/distance.py
+++ lib/mark_score/distance.py
@@ -1,9 +1,6 @@
 from numpy import linalg as la
 def caculate_distance(s):
-    result = []
-    for i in range(len(s)):
-        result.append(la.norm(s[i]))
-    return result
+    return la.norm(s)
 
 
 
