18a4dd42d14d5856b808554052cde62dc334c74d
hieu28022000 <18520748@gm.uit.edu.vn>
2021-05-16 21:57:14 +0700

    update methods mark score

--- lib/mark_score/mark.py
+++ lib/mark_score/mark.py
@@ -1,4 +1,10 @@
 from numpy import double
+
+def mark_demand(data_demand, request_demand):
+    if int(data_demand) == int(request_demand):
+        return 10
+    return 0
+
 def mark_price(value_attr, request):
     money = 100000000
     result = request - int(value_attr)
@@ -23,28 +29,6 @@ def mark_price(value_attr, request):
     else:
         return 0
 
-def mark_floor(value_attr,request):
-    floor = 0
-    floor = request - int(value_attr)
-    if(floor==0):
-        return 10
-    elif(floor==1 or floor==-1):
-        return 9
-    elif (floor == 2 or floor == -2):
-        return 8
-    elif (floor == 3 or floor == -3):
-        return 7
-    elif (floor == 4 or floor == -4):
-        return 6
-    elif (floor == 5 or floor == -5):
-        return 5
-    else:
-        return 0
-def mark_view(value_attr, request):
-    for j in range(len(request)):
-        if(request[j]==value_attr):
-            return double(10/len(request))
-    return 0
 def mark_area(value_attr, request):
     result = request - float(value_attr)
     if(abs(result)<5):
@@ -53,6 +37,12 @@ def mark_area(value_attr, request):
         return 5
     else:
         return 0
+
+def mark_location(data_location, request_location):
+    if int(data_location) == int(request_location):
+        return 10
+    return 0
+
 def mark_room(value_attr, request):
     result = request - int(value_attr)
     if(abs(result)==0):
@@ -61,6 +51,7 @@ def mark_room(value_attr, request):
         return 5
     else:
         return 0
+
 def mark_wc(value_attr, request):
     result = request - int(value_attr)
     if(abs(result)==0):
@@ -79,9 +70,31 @@ def mark_juridical(value_attr, request):
         return 10
     else:
         return 0
+
+def mark_view(value_attr, request):
+    if(request==value_attr):
+        return 10
+    return 0
+
+def mark_floor(value_attr,request):
+    floor = 0
+    floor = request - int(value_attr)
+    if(floor==0):
+        return 10
+    elif(floor==1 or floor==-1):
+        return 9
+    elif (floor == 2 or floor == -2):
+        return 8
+    elif (floor == 3 or floor == -3):
+        return 7
+    elif (floor == 4 or floor == -4):
+        return 6
+    elif (floor == 5 or floor == -5):
+        return 5
+    else:
+        return 0
+
 def mark_hot(value_attr):
     if(value_attr == 'true'):
         return 10
     return 0
-
-print(mark_area(70, 80))
