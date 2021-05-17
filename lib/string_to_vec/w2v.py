8ef5fe16bcda30d92a9fa914ca768d809f0782da
hieu28022000 <18520748@gm.uit.edu.vn>
2021-05-17 16:03:56 +0700

    update methods word embedding

--- lib/string_to_vec/w2v.py
+++ lib/string_to_vec/w2v.py
@@ -24,6 +24,16 @@ def Get_line(num_line, dataset):
     line = line.values[0]
     return line
 
+# index of location in dictionary
+def Search_location(location):
+    loc_dict =  open('./lib/config/location_dict.txt', encoding='utf8').read().split('\n')
+    
+    for index in range(len(loc_dict)):
+        if location == loc_dict[index]:
+            return index
+    return 19
+
+
 # Convert an object to vector
 def Convert_obj2vector(line, dataset):
     if line[4] == 'Shophouse':
@@ -35,7 +45,6 @@ def Convert_obj2vector(line, dataset):
     juridical = dataset['Pháp lý sở hữu']
     view = dataset['View']
     dict_cls = indexing(cls)
-    dict_address = indexing(address)
     dict_furniture = indexing(furniture)
     dict_juridical = indexing(juridical)
     dict_view = indexing(view)
@@ -44,7 +53,7 @@ def Convert_obj2vector(line, dataset):
     vector[0] = str2vec(line[0], dict_cls)
     vector[1] = int(line[1])
     vector[2] = float(line[2])
-    vector[3] = str2vec(line[3], dict_address)
+    vector[3] = Search_location(line[3])
     vector[4] = int(line[4])
     vector[5] = int(line[5])
     vector[6] = str2vec(line[6], dict_furniture)
