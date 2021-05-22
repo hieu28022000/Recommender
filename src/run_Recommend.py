56b9141ba7803a2745fff1e40c9cff7185680523
hieu28022000 <18520748@gm.uit.edu.vn>
2021-05-22 11:21:57 +0700

    Update source

--- src/run_Recommend.py
+++ src/run_Recommend.py
@@ -1,16 +1,15 @@
 import __init__
-from src.source import Get_request, run_source, Load_dataset, Get_line
-from libs.distance.attribute_distance import location_distance
+from src.source import Get_request, run_source, Load_dataset
 
 map_path = './dataset/map.json'
-dataset_path = './dataset/Myhub_dataset.csv'
+dataset_path = './dataset/Myhub_datasetv2.csv'
 cfg_path = './libs/config/basic.json'
 onoff_file_path = './src/on_off.txt'
-
 
 request_input = Get_request()
 print(request_input, '\n\n')
 run_source(request_input, map_path, dataset_path, cfg_path)
+
 
 
 
