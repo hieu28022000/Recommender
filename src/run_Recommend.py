efcb790ef448b9fd031f8cd788f64f0d6f6bd3c4
hieu28022000 <18520748@gm.uit.edu.vn>
2021-05-24 12:53:07 +0700

    Update source

--- src/run_Recommend.py
+++ src/run_Recommend.py
@@ -6,18 +6,6 @@ dataset_Apartment = './dataset/Apartment.csv'
 dataset_Service_floor = './dataset/Service_floor.csv'
 cfg_path = './libs/config/basic.json'
 
-print("Apartmen{0} or Service floor{1}: ")
-service = int(input())
-
 request_input = Get_request()
 print(request_input, '\n\n')
-if service == 0:
-    run_source(request_input, map_path, dataset_Apartment, cfg_path)
-else:
-    run_source(request_input, map_path, dataset_Service_floor, cfg_path)
-
-
-
-
-
-
+run_source(request_input, map_path, dataset_Apartment, cfg_path)
