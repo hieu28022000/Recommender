2be01bfee772a52851827d294a2a4a3ef2799f68
Queanh-nl <83811564+Queanh-nl@users.noreply.github.com>
2021-06-03 09:00:46 +0700

    comment and test in run_Recommender

--- src/run_Recommend.py
+++ src/run_Recommend.py
@@ -1,5 +1,6 @@
 import __init__
 from src.source import Get_request, run_source, Load_dataset
+from libs.dataset.update_dataset import update_dataset
 
 map_path = './dataset/map.json'
 dataset_Apartment = './dataset/Apartment.csv'
@@ -11,8 +12,8 @@ print(request_input, '\n')
 
 run_source(request_input, map_path, dataset_Apartment, cfg_path)
 
+# line = ['Thuê', '8000000', '67.2', 'thủ đức', '2', '2', 'Full', 'HDMB', 'Đông Bắc', '8.0', 'FALSE', 'C.08.06-4s linh đông']
 
+# data = Load_dataset(dataset_Apartment)
 
-
-
-
+# update_dataset(line, data, dataset_Apartment)
